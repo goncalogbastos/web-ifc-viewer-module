79aeba578c86cc0c6614903c59e430ac49d58f4e
goncalogbastos <bastos.g.goncalo@gmail.com>
2022-10-20 18:07:02 +0100

    IFc to report feature

--- app.js
+++ app.js
@@ -49,8 +49,8 @@ async function loadIfc(url) {
   //Load Model  
   model = await viewer.IFC.loadIfcUrl(url);
   await viewer.shadowDropper.renderShadow(model.modelID); 
-  viewer.context.renderer.postProduction.active = active;
-  
+  //viewer.context.renderer.postProduction.active = active;
+  ifcReport();
   /*
   model.removeFromParent();
   togglePickable(model, false);
@@ -86,37 +86,64 @@ async function loadIfc(url) {
 /////////////////////////////////////////////////////////////////////////////////////////////
 // IFC REPORT
 /////////////////////////////////////////////////////////////////////////////////////////////
-
-const walls = await viewer.IFC.getAllItemsOfType(model.modelID, IFCWALLSTANDARDCASE, true);
-const table = document.getElementById('tbody');
-const body = table.querySelector('tbody');
-for(const wall of walls){
-  createWallNameEntry(body, wall);
-  for(const propertyName in wall){
-    const propertyValue = wall[propertyName];
+async function ifcReport(){
+  const walls = await viewer.IFC.getAllItemsOfType(model.modelID, IFCWALLSTANDARDCASE, true);
+  const table = document.getElementById('walls-table');
+  const body = table.querySelector('tbody');
+  for(const wall of walls){
+    createWallNameEntry(body, wall);
+    for(const propertyName in wall){
+      const propertyValue = wall[propertyName];
+      addPropertyEntry(body, propertyName, propertyValue);    
+    }
   }
-
+  const exportButton = document.getElementById('export');
+  exportButton.onclick = () => {
+    const book = XLSX.utils.table_to_book(table);
+    XLSX.writeFile(book, "Walls_Table.xlsx");
+ }
 }
 
-function createWallNameEntry(table, wall){
+
+function createWallNameEntry(table, wall) {
   const row = document.createElement('tr');
   table.appendChild(row);
 
   const wallName = document.createElement('td');
   wallName.colSpan = 2;
-  wallName.textContent = "Wall " + wall.GlobalId.value;
+  wallName.textContent = 'Wall ' + wall.GlobalId.value;
   row.appendChild(wallName);
+}
 
+function addPropertyEntry(table, name, value) {
+  const row = document.createElement('tr');
+  table.appendChild(row);
+
+  const propertyName = document.createElement('td');
+  name = decodeIFCString(name);
+  propertyName.textContent = name;
+  row.appendChild(propertyName);
+
+  if(value === null || value === undefined) value = "Unknown";
+  if(value.value) value = value.value;
+  value = decodeIFCString(value);
+
+  const propertyValue = document.createElement('td');
+  propertyValue.textContent = value;
+  row.appendChild(propertyValue);
+}
+
+function decodeIFCString (ifcString) {
+  const ifcUnicodeRegEx = /\\X2\\(.*?)\\X0\\/uig;
+  let resultString = ifcString;
+  let match = ifcUnicodeRegEx.exec (ifcString);
+  while (match) {
+      const unicodeChar = String.fromCharCode (parseInt (match[1], 16));
+      resultString = resultString.replace (match[0], unicodeChar);
+      match = ifcUnicodeRegEx.exec (ifcString);
   }
-
-function createPropertyEntry(tableBody, name, value){
-    const row = document.createElement('td');
-    tableBody.appendChild(row);
-
-    const propertyName = document.createElement('td');
-  }
-
-
+  return resultString;
+}
 
 
 /*
